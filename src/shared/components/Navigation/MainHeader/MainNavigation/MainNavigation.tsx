d80960fc2f8a0010907108c07236a9b4ea9af5a6
Yaron Veg <yaronveg1@gmail.com>
2022-06-10 18:47:24 +0300

    add side drawer with links to main navigation

--- src/shared/components/Navigation/MainHeader/MainNavigation/MainNavigation.tsx
+++ src/shared/components/Navigation/MainHeader/MainNavigation/MainNavigation.tsx
@@ -1,23 +1,31 @@
 import { Link } from "react-router-dom";
+import SideDrawer from "../../SideDrawer/SideDrawer";
 import MainHeader from "../MainHeader";
 import NavLinks from "../NavLinks/NavLinks";
 import "./MainNavigation.css";
 
 const MainNavigation = () => {
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">YourPlaces</Link>
-      </h1>
-      <nav className="main-navigation__header-nav">
-        <NavLinks />
-      </nav>
-    </MainHeader>
+    <>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlaces</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </>
   );
 };
 export default MainNavigation;
