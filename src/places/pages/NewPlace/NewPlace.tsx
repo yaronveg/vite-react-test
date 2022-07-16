62513ccee8493279404959a38e5a3f7c4d727a03
Yaron Veg <Yaronveg1@gmail.com>
2022-07-16 18:20:12 +0300

    add form submit and address input

--- src/places/pages/NewPlace/NewPlace.tsx
+++ src/places/pages/NewPlace/NewPlace.tsx
@@ -47,8 +47,13 @@ const NewPlace = () => {
     [dispatch]
   );
 
+  const submitPlaceHandler = (event) => {
+    event.preventDefault();
+    console.log("NewPlace: formState inputs:", formState.inputs); // TODO: send this to backend
+  };
+
   return (
-    <form className="place-form">
+    <form className="place-form" onSubmit={submitPlaceHandler}>
       <Input
         id="title"
         element="input"
@@ -64,6 +69,14 @@ const NewPlace = () => {
         label="Description"
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="Please enter a valid description (at least 5 characters)"
+        onInput={inputHandler}
+      />
+      <Input
+        id="address"
+        element="input"
+        label="Address"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid address"
         onInput={inputHandler}
       />
       <Button type="submit" disabled={!formState.isValid}>
