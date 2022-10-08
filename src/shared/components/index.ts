231ffdfca1c1112b73b84fe50d925774a2f0a8c3
Yaron Veg <Yaronveg1@gmail.com>
2022-10-08 22:44:17 +0300

    more error handling, export all shared components from index file

---
+++ src/shared/components/index.ts
@@ -0,0 +1,16 @@
+// form elements
+export { default as Button } from "./FormElements/Button/Button";
+export { default as Input } from "./FormElements/Input/Input";
+
+// navigation
+export { default as MainHeader } from "./Navigation/MainHeader/MainHeader";
+export { default as SideDrawer } from "./Navigation/SideDrawer/SideDrawer";
+
+// UIElements
+export { default as Avatar } from "./UIElements/Avatar/Avatar";
+export { default as Backdrop } from "./UIElements/Backdrop/Backdrop";
+export { default as Card } from "./UIElements/Card/Card";
+export { default as ErrorModal } from "./UIElements/ErrorModal/ErrorModal";
+export { default as LoadingSpinner } from "./UIElements/LoadingSpinner/LoadingSpinner";
+export { default as Map } from "./UIElements/Map/Map";
+export { default as Modal } from "./UIElements/Modal/Modal";
--- src/user/pages/Auth/Auth.tsx
+++ src/user/pages/Auth/Auth.tsx
@@ -1,18 +1,19 @@
 import { useContext, useState } from "react";
-import Button from "../../../shared/components/FormElements/Button/Button";
-import Input from "../../../shared/components/FormElements/Input/Input";
-import Card from "../../../shared/components/UIElements/Card/Card";
-import LoadingSpinner from "../../../shared/components/UIElements/LoadingSpinner/LoadingSpinner";
-import ErrorModal from "../../../shared/components/UIElements/ErrorModal/ErrorModal";
+import {
+  Button,
+  Card,
+  Input,
+  LoadingSpinner,
+  ErrorModal,
+} from "../../../shared/components";
+import { AuthContext } from "../../../shared/context/auth-context";
 import { useForm } from "../../../shared/hooks/form-hook";
 import {
   VALIDATOR_EMAIL,
-  VALIDATOR_MAXLENGTH,
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
 } from "../../../shared/utils/validation";
 import "./Auth.css";
-import { AuthContext } from "../../../shared/context/auth-context";
 
 const Auth = () => {
   const [formState, inputHandler, setFormData] = useForm(
@@ -49,6 +50,9 @@ const Auth = () => {
         });
 
         const resData = await res.json();
+        if (!resData.ok) {
+          throw new Error(resData.message);
+        }
         console.log(resData);
         setIsLoading(false);
         auth.login();
@@ -76,49 +80,56 @@ const Auth = () => {
     setIsLoginMode((prevMode) => !prevMode);
   };
 
+  const errorHandler = () => {
+    setServerError(null);
+  };
+
   return (
-    <Card className="authentication">
-      {isLoading && <LoadingSpinner asOverlay />}
-      <h2>Login Required</h2>
-      <hr />
-      <form onSubmit={submitAuthHandler}>
-        {!isLoginMode && (
+    <>
+      <ErrorModal error={serverError} onClear={errorHandler} />
+      <Card className="authentication">
+        {isLoading && <LoadingSpinner asOverlay />}
+        <h2>Login Required</h2>
+        <hr />
+        <form onSubmit={submitAuthHandler}>
+          {!isLoginMode && (
+            <Input
+              id="name"
+              element="input"
+              type="text"
+              label="Your Name"
+              validators={[VALIDATOR_REQUIRE()]}
+              errorText="Please enter your name."
+              onInput={inputHandler}
+            />
+          )}
           <Input
-            id="name"
+            id="email"
             element="input"
-            type="text"
-            label="Your Name"
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText="Please enter your name."
+            type="email"
+            label="Email"
+            validators={[VALIDATOR_REQUIRE(), VALIDATOR_EMAIL()]}
+            errorText="Please enter a valid email"
             onInput={inputHandler}
           />
-        )}
-        <Input
-          id="email"
-          element="input"
-          type="email"
-          label="Email"
-          validators={[VALIDATOR_REQUIRE(), VALIDATOR_EMAIL()]}
-          errorText="Please enter a valid email"
-          onInput={inputHandler}
-        />
-        <Input
-          id="password"
-          element="input"
-          type="password"
-          label="Password"
-          validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(6)]}
-          errorText="Password must contain at least 6 characters."
-          onInput={inputHandler}
-        />
-        <Button type="submit" disabled={!formState.isValid}>
-          {isLoginMode ? "LOGIN" : "SIGNUP"}
+          <Input
+            id="password"
+            element="input"
+            type="password"
+            label="Password"
+            validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(6)]}
+            errorText="Password must contain at least 6 characters."
+            onInput={inputHandler}
+          />
+          <Button type="submit" disabled={!formState.isValid}>
+            {isLoginMode ? "LOGIN" : "SIGNUP"}
+          </Button>
+        </form>
+        <Button inverse onClick={switchModeHandler}>
+          SWITCH TO {isLoginMode ? "SIGNUP" : "LOGIN"}
         </Button>
-      </form>
-      <Button inverse onClick={switchModeHandler}>
-        SWITCH TO {isLoginMode ? "SIGNUP" : "LOGIN"}
-      </Button>
-    </Card>
+      </Card>
+    </>
   );
 };
 
