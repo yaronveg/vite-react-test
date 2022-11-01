fd9d2416aa50d4d5dadfd3fa21e9c825e4176c32
Yaron Veg <Yaronveg1@gmail.com>
2022-11-01 21:23:23 +0200

    split auth context to logged in and out and remove number from user id

--- src/shared/context/auth-context.ts
+++ src/shared/context/auth-context.ts
@@ -1,11 +1,17 @@
 import { createContext } from "react";
-import { IdType } from "../../user/interfaces";
 
-export interface AuthContextInterface {
-  isLoggedIn: boolean;
-  userId: IdType | null;
+export type AuthContextInterface = AuthContextLoggedIn | AuthContextLoggedOut;
+interface AuthContextBase {
   login: (uid: string) => void;
   logout: () => void;
+}
+interface AuthContextLoggedIn extends AuthContextBase {
+  isLoggedIn: true;
+  userId: string;
+}
+interface AuthContextLoggedOut extends AuthContextBase {
+  isLoggedIn: false;
+  userId: null;
 }
 export const AuthContext = createContext<AuthContextInterface>({
   isLoggedIn: false,
