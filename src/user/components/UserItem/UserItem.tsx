ae4aed158d6e2f40c183abd72e9b432322869cfc
Yaron Veg <Yaronveg1@gmail.com>
2022-11-01 21:24:04 +0200

    user avatar from server

--- src/user/components/UserItem/UserItem.tsx
+++ src/user/components/UserItem/UserItem.tsx
@@ -20,7 +20,7 @@ const UsersItem = ({
       <Card className="user-item__content">
         <Link to={`/${id}/places`}>
           <div className="user-item__image">
-            <Avatar image={image} alt={name} />
+            <Avatar image={`http://localhost:5000/${image}`} alt={name} />
           </div>
           <div className="user-item__info">
             <h2>{name}</h2>
