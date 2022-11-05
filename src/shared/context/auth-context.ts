dd116e1bc8dcbf495109fa5dc6969a0705815268
Yaron Veg <Yaronveg1@gmail.com>
2022-11-05 23:20:12 +0200

    fir new types of auth context and default

--- src/shared/context/auth-context.ts
+++ src/shared/context/auth-context.ts
@@ -2,20 +2,23 @@ import { createContext } from "react";
 
 export type AuthContextInterface = AuthContextLoggedIn | AuthContextLoggedOut;
 interface AuthContextBase {
-  login: (uid: string) => void;
+  login: (uid: string, token: string) => void;
   logout: () => void;
 }
 interface AuthContextLoggedIn extends AuthContextBase {
   isLoggedIn: true;
   userId: string;
+  token: string;
 }
 interface AuthContextLoggedOut extends AuthContextBase {
   isLoggedIn: false;
   userId: null;
+  token: null;
 }
 export const AuthContext = createContext<AuthContextInterface>({
   isLoggedIn: false,
   userId: null,
+  token: null,
   login: () => {},
   logout: () => {},
 });
