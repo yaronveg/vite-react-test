f6c793083e89df1a18f31b868dbcdef871400c73
Yaron Veg <Yaronveg1@gmail.com>
2022-11-11 12:44:43 +0200

    add types to avatar

--- src/shared/components/UIElements/Avatar/Avatar.tsx
+++ src/shared/components/UIElements/Avatar/Avatar.tsx
@@ -1,6 +1,14 @@
+import React from "react";
 import "./Avatar.css";
 
-const Avatar = (props) => {
+const Avatar = (props: {
+  className?: string;
+  style?: React.CSSProperties;
+  image: string;
+  alt: string;
+  width: string | number;
+  height: string | number;
+}) => {
   return (
     <div className={`avatar ${props.className}`} style={props.style}>
       <img
