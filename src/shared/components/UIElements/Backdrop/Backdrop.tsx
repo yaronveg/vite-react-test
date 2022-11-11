1078d140ae54da9ebe516f046ec47c6e82eda4c1
Yaron Veg <Yaronveg1@gmail.com>
2022-11-11 12:38:56 +0200

    add types to backdrop

--- src/shared/components/UIElements/Backdrop/Backdrop.tsx
+++ src/shared/components/UIElements/Backdrop/Backdrop.tsx
@@ -1,10 +1,12 @@
 import { createPortal } from "react-dom";
 import "./Backdrop.css";
 
-const Backdrop = (props) => {
+const Backdrop = (props: {
+  onClick: React.MouseEventHandler<HTMLDivElement>;
+}) => {
   const content = <div className="backdrop" onClick={props.onClick}></div>;
   const portal = document.getElementById("backdrop-hook");
 
-  return createPortal(content, portal);
+  return createPortal(content, portal as Element);
 };
 export default Backdrop;
