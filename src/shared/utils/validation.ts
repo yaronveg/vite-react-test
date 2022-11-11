7c293ad7267df695297d8d5bda486e7abd7d6974
Yaron Veg <Yaronveg1@gmail.com>
2022-11-11 11:43:15 +0200

    add types to the validation file - wip

--- src/shared/utils/validation.ts
+++ src/shared/utils/validation.ts
@@ -8,41 +8,36 @@ const VALIDATOR_TYPE_FILE = "FILE";
 
 // TODO: create universal type for validators
 export const VALIDATOR_REQUIRE = () => ({ type: VALIDATOR_TYPE_REQUIRE });
-export const VALIDATOR_MINLENGTH = (val) => ({
+export const VALIDATOR_MINLENGTH = (val: number) => ({
   type: VALIDATOR_TYPE_MINLENGTH,
   val: val,
 });
-export const VALIDATOR_MAXLENGTH = (val) => ({
+export const VALIDATOR_MAXLENGTH = (val: number) => ({
   type: VALIDATOR_TYPE_MAXLENGTH,
   val: val,
 });
-export const VALIDATOR_MIN = (val) => ({ type: VALIDATOR_TYPE_MIN, val: val });
-export const VALIDATOR_MAX = (val) => ({ type: VALIDATOR_TYPE_MAX, val: val });
+export const VALIDATOR_MIN = (val: number) => ({
+  type: VALIDATOR_TYPE_MIN,
+  val: val,
+});
+export const VALIDATOR_MAX = (val: number) => ({
+  type: VALIDATOR_TYPE_MAX,
+  val: val,
+});
 export const VALIDATOR_EMAIL = () => ({ type: VALIDATOR_TYPE_EMAIL });
 export const VALIDATOR_FILE = () => ({ type: VALIDATOR_TYPE_FILE });
 
-export const validation = (value, validators) => {
+type ValidatorConfig = (val?: string | number) => {
+  type: string;
+  val?: string | number;
+};
+
+export const validation = (
+  value: string | number,
+  validators: ValidatorConfig[]
+) => {
   let isValid = true;
   for (const validator of validators) {
-    // Original code used "if" statements.
-    // if (validator.type === VALIDATOR_TYPE_REQUIRE) {
-    //   isValid = isValid && value.trim().length > 0;
-    // }
-    // if (validator.type === VALIDATOR_TYPE_MINLENGTH) {
-    //   isValid = isValid && value.trim().length >= validator.val;
-    // }
-    // if (validator.type === VALIDATOR_TYPE_MAXLENGTH) {
-    //   isValid = isValid && value.trim().length <= validator.val;
-    // }
-    // if (validator.type === VALIDATOR_TYPE_MIN) {
-    //   isValid = isValid && +value >= validator.val;
-    // }
-    // if (validator.type === VALIDATOR_TYPE_MAX) {
-    //   isValid = isValid && +value <= validator.val;
-    // }
-    // if (validator.type === VALIDATOR_TYPE_EMAIL) {
-    //   isValid = isValid && /^\S+@\S+\.\S+$/.test(value);
-    // }
     switch (validator.type) {
       case VALIDATOR_TYPE_REQUIRE:
         isValid = isValid && value.trim().length > 0;
