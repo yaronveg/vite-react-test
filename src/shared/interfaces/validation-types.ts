aceb917ef48ad081c00168649a9088f61dd4968a
Yaron Veg <Yaronveg1@gmail.com>
2022-11-19 10:13:46 +0200

    add validation types file

---
+++ src/shared/interfaces/validation-types.ts
@@ -0,0 +1,41 @@
+export const VALIDATOR_TYPE_REQUIRE = "REQUIRE";
+export const VALIDATOR_TYPE_MINLENGTH = "MINLENGTH";
+export const VALIDATOR_TYPE_MAXLENGTH = "MAXLENGTH";
+export const VALIDATOR_TYPE_MIN = "MIN";
+export const VALIDATOR_TYPE_MAX = "MAX";
+export const VALIDATOR_TYPE_EMAIL = "EMAIL";
+export const VALIDATOR_TYPE_FILE = "FILE";
+
+type ValidatorKey =
+  | typeof VALIDATOR_TYPE_REQUIRE
+  | typeof VALIDATOR_TYPE_MINLENGTH
+  | typeof VALIDATOR_TYPE_MAXLENGTH
+  | typeof VALIDATOR_TYPE_MIN
+  | typeof VALIDATOR_TYPE_MAX
+  | typeof VALIDATOR_TYPE_EMAIL
+  | typeof VALIDATOR_TYPE_FILE;
+
+export interface ValidatorObject {
+  type: ValidatorKey;
+  limiter?: number;
+}
+
+interface ValidatorConfigBase<A, B> {
+  type: A;
+  limiter: B;
+}
+
+type ValidatorConfigRequired = ValidatorConfigBase<"REQUIRE", undefined>;
+type ValidatorConfigMinlength = ValidatorConfigBase<"MINLENGTH", number>;
+type ValidatorConfigMaxlength = ValidatorConfigBase<"MAXLENGTH", number>;
+type ValidatorConfigMin = ValidatorConfigBase<"MIN", number>;
+type ValidatorConfigMax = ValidatorConfigBase<"MAX", number>;
+type ValidatorConfigEmail = ValidatorConfigBase<"EMAIL", number>;
+
+export type ValidatorConfig =
+  | ValidatorConfigRequired
+  | ValidatorConfigMinlength
+  | ValidatorConfigMaxlength
+  | ValidatorConfigMin
+  | ValidatorConfigMax
+  | ValidatorConfigEmail;
