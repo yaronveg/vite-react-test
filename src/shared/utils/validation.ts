2c0bcb3d3da982c82a2cafd68f81b4f335819d1b
Yaron Veg <Yaronveg1@gmail.com>
2022-11-19 10:14:13 +0200

    use new validation file. validators value are now called limiters

--- src/shared/utils/validation.ts
+++ src/shared/utils/validation.ts
@@ -1,55 +1,46 @@
-const VALIDATOR_TYPE_REQUIRE = "REQUIRE";
-const VALIDATOR_TYPE_MINLENGTH = "MINLENGTH";
-const VALIDATOR_TYPE_MAXLENGTH = "MAXLENGTH";
-const VALIDATOR_TYPE_MIN = "MIN";
-const VALIDATOR_TYPE_MAX = "MAX";
-const VALIDATOR_TYPE_EMAIL = "EMAIL";
-const VALIDATOR_TYPE_FILE = "FILE";
+import {
+  ValidatorObject,
+  VALIDATOR_TYPE_REQUIRE,
+  VALIDATOR_TYPE_MINLENGTH,
+  VALIDATOR_TYPE_MAXLENGTH,
+  VALIDATOR_TYPE_MIN,
+  VALIDATOR_TYPE_MAX,
+  VALIDATOR_TYPE_EMAIL,
+  VALIDATOR_TYPE_FILE,
+  ValidatorConfig,
+} from "./../interfaces/validation-types";
 
 // TODO: create universal type for validators
-export const VALIDATOR_REQUIRE = () => ({ type: VALIDATOR_TYPE_REQUIRE });
-export const VALIDATOR_MINLENGTH = (val: number) => ({
+export const VALIDATOR_REQUIRE = (): ValidatorObject => ({
+  type: VALIDATOR_TYPE_REQUIRE,
+});
+export const VALIDATOR_MINLENGTH = (limiter: number): ValidatorObject => ({
   type: VALIDATOR_TYPE_MINLENGTH,
-  val: val,
+  limiter,
 });
-export const VALIDATOR_MAXLENGTH = (val: number) => ({
+export const VALIDATOR_MAXLENGTH = (limiter: number): ValidatorObject => ({
   type: VALIDATOR_TYPE_MAXLENGTH,
-  val: val,
+  limiter,
 });
-export const VALIDATOR_MIN = (val: number) => ({
+export const VALIDATOR_MIN = (limiter: number): ValidatorObject => ({
   type: VALIDATOR_TYPE_MIN,
-  val: val,
+  limiter,
 });
-export const VALIDATOR_MAX = (val: number) => ({
+export const VALIDATOR_MAX = (limiter: number): ValidatorObject => ({
   type: VALIDATOR_TYPE_MAX,
-  val: val,
+  limiter,
 });
-export const VALIDATOR_EMAIL = () => ({ type: VALIDATOR_TYPE_EMAIL });
-export const VALIDATOR_FILE = () => ({ type: VALIDATOR_TYPE_FILE });
-
-interface ValidatorConfigBase<A, B> {
-  type: A;
-  value: B;
-}
-type ValidatorConfigRequired = ValidatorConfigBase<"REQUIRE", undefined>;
-type ValidatorConfigMinlength = ValidatorConfigBase<"MINLENGTH", number>;
-type ValidatorConfigMaxlength = ValidatorConfigBase<"MAXLENGTH", number>;
-type ValidatorConfigMin = ValidatorConfigBase<"MIN", number>;
-type ValidatorConfigMax = ValidatorConfigBase<"MAX", number>;
-type ValidatorConfigEmail = ValidatorConfigBase<"EMAIL", number>;
-
-export type ValidatorConfig =
-  | ValidatorConfigRequired
-  | ValidatorConfigMinlength
-  | ValidatorConfigMaxlength
-  | ValidatorConfigMin
-  | ValidatorConfigMax
-  | ValidatorConfigEmail;
+export const VALIDATOR_EMAIL = (): ValidatorObject => ({
+  type: VALIDATOR_TYPE_EMAIL,
+});
+export const VALIDATOR_FILE = (): ValidatorObject => ({
+  type: VALIDATOR_TYPE_FILE,
+});
 
 export const validation = (
   value: string | number,
   validators: ValidatorConfig[]
-) => {
+): boolean => {
   let isValid = true;
   for (const validator of validators) {
     switch (validator.type) {
@@ -57,16 +48,18 @@ export const validation = (
         isValid = isValid && (!!value || value === 0);
         break;
       case VALIDATOR_TYPE_MINLENGTH:
-        isValid = isValid && (value as string).trim().length >= validator.value;
+        isValid =
+          isValid && (value as string).trim().length >= validator.limiter;
         break;
       case VALIDATOR_TYPE_MAXLENGTH:
-        isValid = isValid && (value as string).trim().length <= validator.value;
+        isValid =
+          isValid && (value as string).trim().length <= validator.limiter;
         break;
       case VALIDATOR_TYPE_MIN:
-        isValid = isValid && +value >= validator.value;
+        isValid = isValid && +value >= validator.limiter;
         break;
       case VALIDATOR_TYPE_MAX:
-        isValid = isValid && +value <= validator.value;
+        isValid = isValid && +value <= validator.limiter;
         break;
       case VALIDATOR_TYPE_EMAIL:
         isValid = isValid && /^\S+@\S+\.\S+$/.test(value as string);
