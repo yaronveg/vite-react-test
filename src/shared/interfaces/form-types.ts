9dd33f8c4a6c22eaf49079e7e84d5a33c888a686
Yaron Veg <Yaronveg1@gmail.com>
2022-11-19 12:06:33 +0200

    add form types for actions and hook inputs

--- src/shared/interfaces/form-types.ts
+++ src/shared/interfaces/form-types.ts
@@ -3,3 +3,24 @@ export type InputHandler = (
   value: String | Blob,
   isValid: boolean
 ) => void;
+
+interface HookInput {
+  value: String | Blob | null;
+  isValid: boolean;
+}
+
+export type HookInputs = Record<string, HookInput>;
+
+interface FormActionInputChange {
+  type: "INPUT_CHANGE";
+  value: String | Blob;
+  isValid: boolean;
+  inputId: string;
+}
+interface FormActionSetData {
+  type: "SET_DATA";
+  inputs: HookInputs;
+  formIsValid: boolean;
+}
+
+export type FormActions = FormActionInputChange | FormActionSetData;
